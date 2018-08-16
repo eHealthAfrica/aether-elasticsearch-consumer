cfdd4cc69cb595657c71a6c122627eff7a1f7aea
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-08-16 09:49:22 -0400

    fix: added deeper schema comprehension and unit test

--- consumer/tests/__init__.py
+++ consumer/tests/__init__.py
@@ -13,7 +13,7 @@
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
@@ -21,7 +21,7 @@
 import json
 import pytest
 import os
-from app.main import ESConsumerManager, connect_es
+from app.main import ESConsumerManager, connect_es, ESItemProcessor  # NOQA
 
 
 class _MockConsumerManager(ESConsumerManager):
@@ -70,3 +70,201 @@ def AutoConfigSettings():
     with open(path) as f:
         obj = json.load(f)
         return obj.get('autoconfig_settings')
+
+
+SAMPLE_DOC = {
+    'start': '2018-08-14T13:50:04.064000+01:00',
+    'end': '2018-08-14T13:52:51.024000+01:00',
+    'today': '2018-08-14T00:00:00',
+    'deviceid': '355662090560127',
+    'phonenumber': None,
+    'note_start': None,
+    'acknowledge_intro': 'OK',
+    'residents_module': {
+        'supervisor_name': 'patricia_gauji',
+        'enumerator': 'idris_muazu',
+        'cluster': 'constitution_independence',
+        'respondent_name': 'Rukayyah',
+        'gender': 'female',
+        'age': 41,
+        'connected_discos_supplied': 'no',
+        'use_alternative_power': 'yes',
+        'largest_source_noise': 'generator musica trade traffic',
+        'wish_to_relocate': 'yes',
+        'why_would_you_choose_to_relocat': 'noise power_supply quality_of_light safety traffic',
+        'common_safety_concerns_in_the_market': 'vandalism robbery_personal',
+        'comments': 'None'
+    },
+    'pic': '1534251107462.jpg',
+    'geo': {
+        'latitude': 10.513823509216309,
+        'longitude': 7.4525299072265625,
+        'altitude': 598.2000122070312,
+        'accuracy': 4.699999809265137
+    },
+    'meta': {
+        'instanceID': 'uuid:601e659e-7393-450b-a59d-f80706e7f55d'
+    },
+    'id': '2856a498-1705-4d2f-b083-ba4199619705',
+    '_id': 'residence_questionnaire',
+    '_version': '1'
+}
+
+TYPE_INSTRUCTIONS = {
+    '_meta': {
+        'aet_subscribed_topics': [
+            'Residence_Questionnaire_1_3'
+        ],
+        'aet_geopoint': 'geo_point'
+    }
+}
+
+DOC_SCHEMA = {
+    'type': 'record',
+    'fields': [
+        {
+            'name': 'start',
+            'type': 'string'
+        },
+        {
+            'name': 'end',
+            'type': 'string'
+        },
+        {
+            'name': 'today',
+            'type': 'string'
+        },
+        {
+            'name': 'deviceid',
+            'type': 'string'
+        },
+        {
+            'name': 'phonenumber',
+            'type': 'None'
+        },
+        {
+            'name': 'note_start',
+            'type': 'None'
+        },
+        {
+            'name': 'acknowledge_intro',
+            'type': 'string'
+        },
+        {
+            'name': 'residents_module',
+            'type': {
+                'type': 'record',
+                'fields': [
+                    {
+                        'name': 'supervisor_name',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'enumerator',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'cluster',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'respondent_name',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'gender',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'age',
+                        'type': 'int'
+                    },
+                    {
+                        'name': 'connected_discos_supplied',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'use_alternative_power',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'largest_source_noise',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'wish_to_relocate',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'why_would_you_choose_to_relocat',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'common_safety_concerns_in_the_market',
+                        'type': 'string'
+                    },
+                    {
+                        'name': 'comments',
+                        'type': 'string'
+                    }
+                ],
+                'name': 'Auto_1'
+            }
+        },
+        {
+            'name': 'pic',
+            'type': 'string'
+        },
+        {
+            'name': 'geo',
+            'type': {
+                'type': 'record',
+                'fields': [
+                    {
+                        'name': 'latitude',
+                        'type': 'float'
+                    },
+                    {
+                        'name': 'longitude',
+                        'type': 'float'
+                    },
+                    {
+                        'name': 'altitude',
+                        'type': 'float'
+                    },
+                    {
+                        'name': 'accuracy',
+                        'type': 'float'
+                    }
+                ],
+                'name': 'Auto_2'
+            }
+        },
+        {
+            'name': 'meta',
+            'type': {
+                'type': 'record',
+                'fields': [
+                    {
+                        'name': 'instanceID',
+                        'type': 'string'
+                    }
+                ],
+                'name': 'Auto_3'
+            }
+        },
+        {
+            'name': 'id',
+            'type': 'string'
+        },
+        {
+            'name': 'es_reserved__id',
+            'type': 'string'
+        },
+        {
+            'name': 'es_reserved__version',
+            'type': 'string'
+        }
+    ],
+    'name': 'Auto_0'
+}
