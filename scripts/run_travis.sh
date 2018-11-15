df826e3cbed63f78c1b1245101e53346dcea8c23
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-11-15 14:46:08 +0100

    chore: more cleanup between unit and integration tests

--- scripts/run_travis.sh
+++ scripts/run_travis.sh
@@ -38,7 +38,7 @@ popd
 scripts/run_unit_tests.sh
 sleep 1
 pushd aether-bootstrap
-docker-compose-test down
+docker-compose -f docker-compose-test.yml down
 sleep 5
 scripts/initialise_docker_environment.sh
 cp .env ..
