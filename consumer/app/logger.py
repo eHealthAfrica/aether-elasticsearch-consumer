ab9c4e3c0deb13db17e429631859e505e2f5491e
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-12-19 16:44:42 +0100

    feat: using jsonpath for path comprehension and searching now. Refactored some classes out of main

---
+++ consumer/app/logger.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2018 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import logging
+from . import config
+
+consumer_config = config.get_consumer_config()
+
+
+def get_module_logger(mod_name):
+    logger = logging.getLogger(mod_name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    level = logging.getLevelName(consumer_config.get('log_level'))
+    logger.setLevel(level)
+    return logger
