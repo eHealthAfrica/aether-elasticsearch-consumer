990221213dc016ede79178339302ff5970e78589
obdulia-losantos <obdulia.losantos@gmail.com>
2021-01-20 09:04:20 +0100

    fix: travis tests

--- consumer/entrypoint.sh
+++ consumer/entrypoint.sh
@@ -29,8 +29,11 @@ show_help() {
     bash          : run bash
     build         : build python wheel of library in /dist
     eval          : eval shell command
+
     pip_freeze    : freeze pip dependencies and write to requirements.txt
+
     start         : run application
+
     test_unit     : run tests
     test_lint     : run flake8 tests
     test_coverage : run tests with coverage output
@@ -38,24 +41,31 @@ show_help() {
     """
 }
 
-PYTEST="pytest --cov-report term-missing --cov=app --cov-append -p no:cacheprovider"
+clean_test() {
+    rm -rf .pytest_cache     || true
+    rm -rf tests/__pycache__ || true
+}
 
 test_flake8() {
-    flake8 /code/. --config=/code/conf/extras/flake8.cfg
+    flake8
 }
 
 test_unit() {
-    $PYTEST -m unit
+    clean_test
+
+    pytest -m unit
     cat /code/conf/extras/good_job.txt
-    rm -R .pytest_cache || true
-    rm -rf tests/__pycache__ || true
+
+    clean_test
 }
 
 test_integration() {
-    $PYTEST -m integration
+    clean_test
+
+    pytest -m integration
     cat /code/conf/extras/good_job.txt
-    rm -R .pytest_cache || true
-    rm -rf tests/__pycache__ || true
+
+    clean_test
 }
 
 case "$1" in
@@ -80,17 +90,23 @@ case "$1" in
         python manage.py "${@:2}"
     ;;
 
+    test_lint )
+        test_flake8
+    ;;
+
     test_unit )
         test_flake8
         test_unit "${@:2}"
     ;;
 
-    test_lint )
-        test_flake8
-    ;;
-
     test_integration )
         test_flake8
+        test_integration "${@:2}"
+    ;;
+
+    test_all )
+        test_flake8
+        test_unit "${@:2}"
         test_integration "${@:2}"
     ;;
 
---
+++ scripts/run_travis.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2018 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -Eeuo pipefail
+
+function _on_exit () {
+  docker-compose -f docker-compose-test.yml down -v
+}
+
+function _on_err () {
+  echo "-------------------------"
+  docker-compose -f docker-compose-test.yml logs redis
+  echo "-------------------------"
+  docker-compose -f docker-compose-test.yml logs elasticsearch
+  echo "-------------------------"
+  docker-compose -f docker-compose-test.yml logs kibana
+  echo "-------------------------"
+
+  exit 1
+}
+
+trap '_on_exit' EXIT
+trap '_on_err' ERR
+
+docker-compose -f docker-compose-test.yml up -d elasticsearch kibana redis
+docker-compose -f docker-compose-test.yml build consumer-test
+
+docker-compose -f docker-compose-test.yml run --rm consumer-test test_all
